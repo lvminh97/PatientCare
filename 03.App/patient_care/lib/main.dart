ccc60b1c0ddebe7d1a1214ba9d97ce7aa7d01dab
lvminh97 <lvminh97@gmail.com>
2022-06-23 20:03:10 +0700

    Config build.gradle for Android, link to Firebase
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- 03.App/patient_care/lib/main.dart
+++ 03.App/patient_care/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:patient_care/screen/LoginPage.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   runApp(const MyApp());
 }
 
