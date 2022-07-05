156e1d27c55fbb21ad85cb9fd63c2c6a25c2a68e
lvminh97 <lvminh97@gmail.com>
2022-07-05 12:08:06 +0700

    - Change timer_100ms into timer_5ms - Add lcdDisplay() function to control the information that is displayed on LCD - Add flags for config mode and wifi status - Add DEBUG option for debugging - Implement extended command: ESP finish config mode and Get WiFi status from ESP - Modify code about sensors value updating: only send sensors value to ESP when WiFi is connected and not in config mode
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- 02.Firmware/Atmega328/Atmega328.ino
+++ 02.Firmware/Atmega328/Atmega328.ino
@@ -5,14 +5,17 @@
 #include <MAX30100_PulseOximeter.h>
 #include <DHT.h>
 
+#define DEBUG true
+
 SoftwareSerial esp(ESP_TX, ESP_RX);
 LiquidCrystal LCD(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 PulseOximeter  pox;
 DHT dht(DHT_PIN, DHT11);
 
-unsigned long timer_100ms, timer_1s = 0, timer_5s = 0;
+unsigned long timer_5ms, timer_1s = 0, timer_5s = 0;
 int sosbtn_cnt = 0, cfgbtn_cnt = 0;
 unsigned char sos_status = 0, cur_disp = 0;
+unsigned char is_config_mode = 0, wifi_status = 0, lcd_update = 0;
 
 int heart, spo2, body_temp, temp, humi;
 
@@ -20,11 +23,14 @@ unsigned char inBuff[20], outBuff[20] = {0x84, 0xF0};
 int buffPos = 0;
 
 void setup(){
+#if DEBUG
   Serial.begin(9600);
+#endif
   gpioInit();
   LCD.begin(16, 2);
   LCD.print("Patient Care");
   pox.begin();
+  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
   dht.begin();
   esp.begin(9600);
   // Reset ESP
@@ -33,25 +39,35 @@ void setup(){
   sendCommand(outBuff, 4);
   delay(500);   // wait 500ms
   // ESP connect to WiFi
+  wifi_status = 0xFF;
   outBuff[2] = 0x80;
   outBuff[3] = 0x85;
   sendCommand(outBuff, 4);
+  lcd_update = 1;
 }
 
 void loop(){
   checkReceiveCommand();
-  if(millis() - timer_100ms >= 100){
+  if(is_config_mode == 0){
     pox.update();
+  }
+  // 5ms tasks
+  if(millis() - timer_5ms >= 5){
     checkSOSBtn();
     checkCfgBtn();
-    timer_100ms = millis();
+    lcdDisplay();
+    timer_5ms = millis();
   }
+  // 1s tasks
   if(millis() - timer_1s >= 1000){
-    updateHeartRateSpO2();
+    if(is_config_mode == 0)
+      updateHeartRateSpO2();
     timer_1s = millis();
   }
+  // 5s tasks
   if(millis() - timer_5s >= 5000){
-    updateTempHumi();
+    if(is_config_mode == 0)
+      updateTempHumi();
     timer_5s = millis();
   }
 }
@@ -60,46 +76,83 @@ void updateHeartRateSpO2(){
   heart = pox.getHeartRate();
   spo2 = pox.getSpO2();
   body_temp = analogRead(LM35) / 1023.0 * 500;
-  LCD.clear();
-  LCD.setCursor(0, 0);
-  LCD.print("H:");
-  LCD.print(heart);
-  LCD.print(" ");
-  LCD.print("SpO2:");
-  LCD.print(spo2);
-  LCD.setCursor(0, 1);
-  LCD.print("BTemp:");
-  LCD.print(body_temp);
-  outBuff[2] = 0x82;
-  outBuff[3] = 0x82;
-  outBuff[4] = (heart >> 8) & 0xFF;
-  outBuff[5] = heart & 0xFF;
-  outBuff[6] = spo2;
-  outBuff[7] = body_temp;
-  sendCommand(outBuff, 8);
+  lcd_update = 1;
+  if(wifi_status == 0x03 && is_config_mode == 0){
+    outBuff[2] = 0x82;
+    outBuff[3] = 0x82;
+    outBuff[4] = (heart >> 8) & 0xFF;
+    outBuff[5] = heart & 0xFF;
+    outBuff[6] = spo2;
+    outBuff[7] = body_temp;
+    sendCommand(outBuff, 8);
+  }
 }
 
 void updateTempHumi(){
   temp = dht.readTemperature();
   humi = dht.readHumidity();
-  outBuff[2] = 0x82;
-  outBuff[3] = 0x81;
-  outBuff[4] = temp;
-  outBuff[5] = humi;
-  sendCommand(outBuff, 6);
+  lcd_update = 1;
+  if(wifi_status == 0x03 && is_config_mode == 0){
+    outBuff[2] = 0x82;
+    outBuff[3] = 0x81;
+    outBuff[4] = temp;
+    outBuff[5] = humi;
+    sendCommand(outBuff, 6);
+  }
+}
+
+void lcdDisplay(){
+  if(lcd_update == 0)
+    return;
+  else
+    lcd_update = 0;
+
+  if(wifi_status == 0xFF){
+    LCD.clear();
+    LCD.print("WiFi connecting");
+  }
+  else if(is_config_mode == 1){
+    LCD.clear();
+    LCD.print("Config mode");
+  }
+  else{
+    if(cur_disp == 0){
+      LCD.clear();
+      LCD.setCursor(0, 0);
+      LCD.print("Heart: ");
+      LCD.print(heart);
+      LCD.setCursor(0, 1);
+      LCD.print("SpO2: ");
+      LCD.print(spo2);
+      LCD.print("%");
+    }
+    else if(cur_disp == 1){
+      LCD.clear();
+      LCD.setCursor(0, 0);
+      LCD.print("T: ");
+      LCD.print(temp);
+      LCD.print(" H: ");
+      LCD.print(humi);
+      LCD.setCursor(0, 1);
+      LCD.print("Body temp: ");
+      LCD.print(body_temp);
+    }
+  }
 }
 
 void checkSOSBtn(){
   if(SOS_GET == LOW){
-    if(sosbtn_cnt < 20){
+    if(sosbtn_cnt < 400){
       sosbtn_cnt++;
-      if(sosbtn_cnt == 20){ // press and hold the SOS button in 2s
+      if(sosbtn_cnt == 400){ // press and hold the SOS button in 2s
         sos_status = !sos_status;
-        // send the SOS status to ESP
-        outBuff[2] = 0x82;
-        outBuff[3] = 0x83;
-        outBuff[4] = sos_status;
-        sendCommand(outBuff, 5);
+        if(wifi_status == 0x03 && is_config_mode == 0){
+          // send the SOS status to ESP
+          outBuff[2] = 0x82;
+          outBuff[3] = 0x83;
+          outBuff[4] = sos_status;
+          sendCommand(outBuff, 5);
+        }
       }
     }
   }
@@ -110,15 +163,16 @@ void checkSOSBtn(){
 
 void checkCfgBtn(){
   if(ESP_CFG_GET == LOW){
-    if(cfgbtn_cnt < 20){
+    if(cfgbtn_cnt < 400){
       cfgbtn_cnt++;
-      if(cfgbtn_cnt == 3){    // switch info displayed in LCD
+      if(cfgbtn_cnt == 20){    // switch info displayed in LCD
         cur_disp = (cur_disp + 1) % 2;
       }
-      else if(cfgbtn_cnt == 20){    // enter ESP config mode
-        cur_disp = 2;
+      else if(cfgbtn_cnt == 400){    // enter ESP config mode
+        is_config_mode = !is_config_mode;
+        lcd_update = 1;
         outBuff[2] = 0x80;
-        outBuff[3] = 0x81;
+        outBuff[3] = is_config_mode ? 0x81 : 0x82;
         sendCommand(outBuff, 4);
       }
     }
@@ -131,8 +185,8 @@ void checkCfgBtn(){
 void checkReceiveCommand(){
   if(esp.available()){
     while(esp.available()){
-      inBuff[buffPos++] = (char) esp.read();
-      delay(2);
+      inBuff[buffPos++] = (unsigned char) esp.read();
+      delayMicroseconds(2500); 
     }
   }
   if(buffPos != 0){
@@ -141,14 +195,36 @@ void checkReceiveCommand(){
 }
 
 void processCommand(){
+#if DEBUG
+  Serial.print("Receive: ");
   for(int i = 0; i < buffPos; i++){
     Serial.print(inBuff[i], HEX);
     Serial.print(" ");
   }
   Serial.println();
+#endif
   if(inBuff[0] == 0x84 && inBuff[1] == 0xF0){
     switch(inBuff[2]){
       case 0x81:
+        if(inBuff[3] == 0x82){    // ESP finish config mode
+          is_config_mode = 0;     // exit from config mode
+          // Reset ESP
+          outBuff[2] = 0x80;
+          outBuff[3] = 0x83;
+          sendCommand(outBuff, 4);
+          delay(500);   // wait 500ms
+          // ESP connect to WiFi
+          wifi_status = 0xFF;
+          outBuff[2] = 0x80;
+          outBuff[3] = 0x85;
+          sendCommand(outBuff, 4);
+          lcd_update = 1;
+        }
+        else if(inBuff[3] == 0x84){ // get WiFi status from ESP
+          wifi_status = inBuff[4];
+          if(wifi_status != 0xFF)
+            lcd_update = 1;
+        }
         break;
       case 0x83:
         RELAY1_SET(inBuff[3]);
@@ -160,7 +236,15 @@ void processCommand(){
 }
 
 void sendCommand(unsigned char cmd[], int len){
+#if DEBUG
+  Serial.print("Send: ");
   for(int i = 0; i < len; i++){
-    esp.print(cmd[i]);
+    Serial.print(cmd[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+#endif
+  for(int i = 0; i < len; i++){
+    esp.print((char) cmd[i]);
   }
 }
