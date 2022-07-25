10c63d435ea84a44a103276280dfa822d262657b
lvminh97 <lvminh97@gmail.com>
2022-07-25 18:38:19 +0700

    Add ChartPage screen for showing the chart of heart rate and SpO2
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

---
+++ 03.App/patient_care/lib/screen/ChartPage.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: file_names, must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class ChartPage extends StatelessWidget {
+
+  late BuildContext _context;
+
+  ChartPage({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+
+          ]
+        ),
+      ),
+    );
+  }
+}
