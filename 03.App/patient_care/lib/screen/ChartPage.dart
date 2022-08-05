734d3c7a2df0e67b294d617792dcf0d004fc68a6
lvminh97 <lvminh97@gmail.com>
2022-08-05 18:20:32 +0700

    Show heart rate and SpO2 chart (with generated data)
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- 03.App/patient_care/lib/screen/ChartPage.dart
+++ 03.App/patient_care/lib/screen/ChartPage.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: file_names, must_be_immutable
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:patient_care/widget/Header.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
@@ -8,15 +10,30 @@ class ChartPage extends StatelessWidget {
 
   late BuildContext _context;
   String _type = "heart";
+  List<DataPoint> _data = [];
 
   ChartPage(String type, {Key? key}) : super(key: key){
     _type = type;
   }
   
+  void generateData(){
+    Random rng = Random();
+    int start = 1659694531, cnt = 0;
+    while(cnt < 100){
+      if(_type == "heart") {
+        _data.add(DataPoint(start, rng.nextInt(6) + 68));
+      } else {
+        _data.add(DataPoint(start, rng.nextInt(4) + 89));
+      }
+      start++;
+      cnt++;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     _context = context;
-
+    generateData();
     return WillPopScope(
       onWillPop: () async {
         Navigator.pushReplacementNamed(_context, "/home");
@@ -41,17 +58,19 @@ class ChartPage extends StatelessWidget {
               Container(
                 margin: const EdgeInsets.only(top: 20),
                 child: SfCartesianChart(
-                  primaryXAxis: CategoryAxis(),
-                  series: <LineSeries<Data, String>>[
-                    LineSeries<Data, String>(
-                      dataSource: <Data>[
-                        Data('Tom', 10),
-                        Data('Jane', 7),
-                        Data('Thor', 20),
-                        Data('Bill', 13)
-                      ],
-                      xValueMapper: (Data d, _) => d.name,
-                      yValueMapper: (Data d, _) => d.value
+                  primaryXAxis: CategoryAxis(
+                    isVisible: false
+                  ),
+                  primaryYAxis: NumericAxis(
+                    minimum: 0,
+                    maximum: _type == "heart" ? 120 : 100,
+                  ),
+                  series: [
+                    LineSeries<DataPoint, int>(
+                      dataSource: _data,
+                      xValueMapper: (DataPoint d, _) => d.timestamp,
+                      yValueMapper: (DataPoint d, _) => d.value,
+                      color: Colors.red
                     )
                   ]
                 ),
@@ -64,12 +83,12 @@ class ChartPage extends StatelessWidget {
   }
 }
 
-class Data{
-  String name = "";
+class DataPoint{
+  int timestamp = 0;
   int value = 0;
 
-  Data(String _name, int _value){
-    name = _name;
+  DataPoint(int _ts, int _value){
+    timestamp = _ts;
     value = _value;
   }
 }
